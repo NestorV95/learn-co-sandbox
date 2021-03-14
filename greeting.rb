7ef765fce0f3864e6e4ca169d680f153f64cd287
Nestor Venegas <nestor.venegas95@gmail.com>
2021-03-14 16:00:56 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,16 @@
+def greeting
+  puts "Hi, Ruby Programmer!"
+end
+
+greeting
+
+def greeting_a_person (name)
+  puts "Hello #{name}"
+end
+
+name = "Maria"
+greeting_a_person
+
+def greeting_programmer (name,language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
